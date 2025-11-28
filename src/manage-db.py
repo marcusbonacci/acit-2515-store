caeea4bf3c8a0a0bb610e40ca266e7901700552f
marcusbonacci <marcusbonacci+github@gmail.com>
2025-11-27 23:58:41 -0800

    push

--- src/manage-db.py
+++ src/manage-db.py
@@ -1,27 +1,59 @@
 # Imports
 import csv
+import warnings
 from pathlib import Path
 from sys import argv
 
+from sqlalchemy import select
+
 try:
-    from app import create_app
-    from database import engine
-    from models import Base
+    from database import Session, engine
+    from models import Base, Customer, Product
 except ImportError as e:
     print("IMPORT ERROR", e)
+
+# Variables
+data_files = [
+    "products.csv",
+    "customers.csv"
+]
+
+session = Session()
 
 # Functions
 def create():
     print(f"Creating tables for {engine}")
     Base.metadata.create_all(engine)
 
+
 def drop():
     print(f"Dropping tables for {engine}")
     Base.metadata.drop_all(engine)
 
 def populate():
+
+    drop()
+    create()
+
     print(f"Populating tables for {engine}")
-    pass
+    for file in data_files:
+        try:
+            file_path = Path(__file__).parent.parent / file
+            file_name = str.removesuffix(file, "s.csv")
+            if (not file_path): raise(f"Couldn't resolve file: {file}")
+
+            with open(file_path) as file:
+                data = csv.DictReader(file)
+                for row in data:
+                    item = globals()[file_name.capitalize()](**row)
+                    session.add(item)
+
+                print(f"Commiting {file_name} to database")
+                session.commit()
+
+        except Exception as e:
+            warnings.warn(f"manage-db.py: {e}")
+
 
 # Main
 if __name__ == "__main__":
