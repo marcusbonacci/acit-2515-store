ea1a6b5f731a31815523fc040e1bad1c3fafdc0f
marcusbonacci <marcusbonacci+github@gmail.com>
2025-11-27 22:57:30 -0800

    Mod: Added Argv to manage-db.py

--- src/manage-db.py
+++ src/manage-db.py
@@ -1,14 +1,16 @@
+# Imports
 import csv
 from pathlib import Path
 from sys import argv
 
 try:
     from app import create_app
-    # from database import db
-    from models import Customer
+    from database import engine
+    from models import Base
 except ImportError as e:
     print("IMPORT ERROR", e)
 
+# Functions
 def create():
     print(f"Creating tables for {engine}")
     Base.metadata.create_all(engine)
@@ -21,5 +23,7 @@ def populate():
     print(f"Populating tables for {engine}")
     pass
 
+# Main
 if __name__ == "__main__":
-    args = 
+    if len(argv) > 1:
+        globals()[argv[1]]()
